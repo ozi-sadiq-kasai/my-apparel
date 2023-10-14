b718f55b3390aeb310a01804f1e7a2bb1f4bfc96
ozi <ozisadiqkasai@gmail.com>
2023-10-14 15:41:05 +0100

    modified files

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import "../styles/Navbar.css"
+import "../Styles/Navbar.css"
 import { AiOutlineSearch } from 'react-icons/ai'
 import Badge from '@mui/material/Badge';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined'
